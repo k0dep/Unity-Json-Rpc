2516be596dd55182fa334449d06d558da241e7c8
Stanislav Petrov <sv.petrov@kodep.pro>
2019-05-19 12:11:20 +0300

    added cors allow all origins, methods and headers

--- JsonRpcRequest.cs
+++ JsonRpcRequest.cs
@@ -37,9 +37,17 @@ namespace UnityJsonRpc
         private void Write(object data)
         {
             byte[] buffer = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data, Formatting.Indented, new JsonSerializerSettings { 
-                NullValueHandling = NullValueHandling.Ignore
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize
             }));
             _response.ContentLength64 = buffer.Length;
+            _response.Headers.Add("Content-Type", "application/json");
+            _response.Headers.Add("Access-Control-Allow-Origin", "*");
+            _response.Headers.Add("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            _response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            _response.Headers.Add("Access-Control-Allow-Headers",
+                "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            
             var output = _response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
             output.Close();
